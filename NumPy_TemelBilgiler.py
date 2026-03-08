10aa248ed9c8a4ad5fde5a97ca6fd32c7fa9c854
Serkan Sevmez <serkansvmz2053@gmail.com>
2026-03-08 14:52:12 +0300

    Enhance NumPy_TemelBilgiler.py with array examples
    Added array manipulation examples including sorting, concatenation, and reshaping.

--- NumPy_TemelBilgiler.py
+++ NumPy_TemelBilgiler.py
@@ -1,12 +1,16 @@
 import numpy as np
 import math
 
+#----------Dizi temelleri-----------------------------------
+
 a = np.array([1, 2, 3, 4, 5, 6])
 print(a)
 b=a[3:]
 print(b)
 b[0]=40
 print(a)
+
+#-------Dizi öznitelikleri---------------------------------
 
 a= np.array([(1,2,3),(4,5,6),(7,8,9)])
 print(a)
@@ -18,7 +22,7 @@ print(a.size) # toplam öge sayısı
 print(a.size==math.prod(a.shape)) # True döndürür
 print(a.dtype)
 
-#-------------------------------------------------------------
+#--------Temel bir dizi nasıl oluşturulur------------------
 
 a=np.zeros(4) # Sıfırlardan oluşan 4 elemanlı
 print(a)
@@ -38,6 +42,62 @@ print(a)
 a=np.linspace(0, 10, num=5) # 0 dan 10 a kadar doğrusal artan 5 elemanlı dizi oluşturur 
 print(a)
 
-a=np.ones(5, dtype=np.float64)
+a=np.ones(5, dtype=np.float64) # veri türünü değiştirme
 print(a)
 print(a.dtype)
+
+#------------Öğeleri ekleme, çıkarma ve sıralama-------------
+
+a=np.array([2, 1, 5, 3, 7, 4, 6, 8])
+print(np.sort(a)) # dizinin elemanlarını sıralı bir şekilde yazar
+
+a = np.array([1, 2, 3, 4])
+b = np.array([5, 6, 7, 8])
+print(np.concatenate((a,b))) #İki diziyi birleştirir
+
+x = np.array([[1, 2], [3, 4]])
+y = np.array([[5, 6]])
+print(np.concatenate((x,y), axis=0)) # dikey eksende birleştirir
+print(np.concatenate((x, y.T), axis=1)) # yatay eksende birleştirir ama öncesinde y nin transpozunu almamız gerek
+print(np.concatenate((x, y), axis=None)) # herhangi eksende birleştirmez ögeleri direk sırayla yazar
+
+#-----------Bir dizinin şeklini ve boyutunu nasıl bilirsiniz-----------
+
+a = np.array([[[0, 1, 2, 3], 
+               [4, 5, 6, 7]], 
+               
+              [[0, 1, 2, 3], 
+               [4, 5, 6, 7]], 
+                
+              [[0 ,1 ,2, 3], 
+               [4, 5, 6, 7]]])
+
+print(a)
+print(a.ndim) # dizinin boyutu
+print(a.size) # dizinin öge sayısı
+print(a.shape) # dizinin şekli
+
+a = np.arange(6)
+print(a)
+b = a.reshape(3, 2) # a dizisini (3,2) lik matrise çevirir
+print(b)
+
+#---------1D dizi 2D diziye nasıl dönüştürülür --------
+
+a = np.array([1, 2, 3, 4, 5, 6])
+print(a)
+print(a.shape) #tek boyutlu bir dizi
+
+a2 = a[np.newaxis, :] # (1,6) lık bir matrise dönüştürür
+print(a2)
+print(a2.shape)
+a2 = a[:, np.newaxis] #  (6,1) lık bir matrise dönüştürür
+print(a2)
+print(a2.shape)
+
+a = np.array([1, 2, 3, 4, 5, 6])
+print(a,"->", a.shape)
+b = np.expand_dims(a, axis=1) # a dizisini sütun vektörüne dönüştürür
+print(b,"->", b.shape )
+c = np.expand_dims(a, axis=0) # a dizisini satır vektörüne dönüştürür
+print(c,"->", c.shape)
